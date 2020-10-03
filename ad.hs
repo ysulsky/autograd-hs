5aad445ba8c2b46c0e570d273c0e2b7fc6cc0a43
Yury Sulsky <yury.sulsky@gmail.com>
2020-10-03 23:32:51 +0100

    Update ad.hs

--- ad.hs
+++ ad.hs
@@ -28,29 +28,29 @@ instance Show a => Show (Tensor a) where
 instance Num a => Num (Tensor a) where
   ta * tb = Tensor { val = val ta * val tb, vjp = \g -> vjp ta (g * tb) ++ vjp tb (g * ta) }
   ta + tb = Tensor { val = val ta + val tb, vjp = \g -> vjp ta g ++ vjp tb g }
-  abs ta = Tensor { val = abs (val ta), vjp = \g -> vjp ta (g * signum ta) }
+  abs ta = Tensor { val = abs (val ta), vjp = vjp ta . (* signum ta) }
   fromInteger v = Tensor { val = fromInteger v, vjp = const [] }
   negate ta = Tensor { val = negate (val ta), vjp = vjp ta . negate }
-  signum ta = Tensor { val = signum (val ta), vjp = \g -> vjp ta 0 }
+  signum ta = Tensor { val = signum (val ta), vjp = vjp ta . const 0 }
 
 instance Fractional a => Fractional (Tensor a) where
-  recip ta = Tensor { val = recip (val ta), vjp = \g -> vjp ta (-g / (ta * ta)) }
+  recip ta = Tensor { val = recip (val ta), vjp = vjp ta . (/ negate (ta * ta)) }
   fromRational v = Tensor { val = fromRational v, vjp = const [] }
 
 instance Floating a => Floating (Tensor a) where
   pi = Tensor { val = pi, vjp = const [] }
-  exp ta = Tensor { val = exp (val ta), vjp = \g -> vjp ta (g * exp ta) }
-  log ta = Tensor { val = log (val ta), vjp = \g -> vjp ta (g / ta) }
-  sin ta = Tensor { val = sin (val ta), vjp = \g -> vjp ta (g * cos ta) }
-  cos ta = Tensor { val = cos (val ta), vjp = \g -> vjp ta (-g * sin ta) }
-  asin ta = Tensor { val = asin (val ta), vjp = \g -> vjp ta (g / sqrt (1 - ta * ta)) }
-  acos ta = Tensor { val = acos (val ta), vjp = \g -> vjp ta (-g / sqrt (1 - ta * ta)) }
-  atan ta = Tensor { val = atan (val ta), vjp = \g -> vjp ta (g / (ta * ta + 1)) }
-  sinh ta = Tensor { val = sinh (val ta), vjp = \g -> vjp ta (g * cosh ta) }
-  cosh ta = Tensor { val = cosh (val ta), vjp = \g -> vjp ta (g * sinh ta) }
-  asinh ta = Tensor { val = asinh (val ta), vjp = \g -> vjp ta (g / sqrt (ta * ta + 1)) }
-  acosh ta = Tensor { val = acosh (val ta), vjp = \g -> vjp ta (g / sqrt (ta * ta - 1)) }
-  atanh ta = Tensor { val = atanh (val ta), vjp = \g -> vjp ta (g / (1 - ta * ta)) }
+  exp ta = Tensor { val = exp (val ta), vjp = vjp ta . (* exp ta) }
+  log ta = Tensor { val = log (val ta), vjp = vjp ta . (/ ta) }
+  sin ta = Tensor { val = sin (val ta), vjp = vjp ta . (* cos ta) }
+  cos ta = Tensor { val = cos (val ta), vjp = vjp ta . (* negate (sin ta)) }
+  asin ta = Tensor { val = asin (val ta), vjp = vjp ta . (/ sqrt (1 - ta * ta)) }
+  acos ta = Tensor { val = acos (val ta), vjp = vjp ta . (/ negate (sqrt (1 - ta * ta))) }
+  atan ta = Tensor { val = atan (val ta), vjp = vjp ta . (/ (ta * ta + 1)) }
+  sinh ta = Tensor { val = sinh (val ta), vjp = vjp ta . (* cosh ta) }
+  cosh ta = Tensor { val = cosh (val ta), vjp = vjp ta . (* sinh ta) }
+  asinh ta = Tensor { val = asinh (val ta), vjp = vjp ta . (/ sqrt (ta * ta + 1)) }
+  acosh ta = Tensor { val = acosh (val ta), vjp = vjp ta . (/ sqrt (ta * ta - 1)) }
+  atanh ta = Tensor { val = atanh (val ta), vjp = vjp ta . (/ (1 - ta * ta)) }
 
 d_dx :: Num a => (Tensor a -> Tensor a) -> Tensor a -> Tensor a
 d_dx f x = head . grad ["x"] . f $ tag "x" x
